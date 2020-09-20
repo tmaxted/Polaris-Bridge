f28ee018c6a7a881843f2a8a644ad3215dd28195
Tom M <38783614+tmaxted@users.noreply.github.com>
2020-09-20 15:17:10 +1200

    created simple noise generation and displayed it on a plane

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	public int mapWidth, mapHeight;
+	public float noiseScale;
+	public bool autoUpdate;
+
+	public void GenerateMap() {
+		float[, ] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+		MapDisplay display = FindObjectOfType<MapDisplay>();
+		display.DrawNoiseMap(noiseMap);
+	}
+}
