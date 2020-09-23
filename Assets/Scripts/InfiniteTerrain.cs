d8b873e0a01275fc3d1a73b61a24c8617e11a7e6
Tom M <38783614+tmaxted@users.noreply.github.com>
2020-09-23 21:12:26 +1200

    Implemented Infinite Terrain generation
    Infinite terrain generation works and chunks are only loaded when close to the viewer, colours are not implemented yet

---
+++ Assets/Scripts/InfiniteTerrain.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteTerrain : MonoBehaviour {
+    public const float maxViewDist = 300;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+
+    int chunkSize;
+    int chunksVisibleInViewDist; // number of chunks around viewer to instantiate
+
+    Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> visibleChunks = new List<TerrainChunk>();
+
+    void Start() {
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisibleInViewDist = Mathf.RoundToInt(maxViewDist) / chunkSize;
+    }
+
+    private void Update() {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisibleChunks();
+    }
+
+    void UpdateVisibleChunks() {
+        for (int i = 0; i < visibleChunks.Count; i++) {
+            visibleChunks[i].SetVisible(false);
+        }
+        visibleChunks.Clear();
+
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for (int yOffset = -chunksVisibleInViewDist; yOffset <= chunksVisibleInViewDist; yOffset++) {
+            for (int xOffset = -chunksVisibleInViewDist; xOffset <= chunksVisibleInViewDist; xOffset++) {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+                if (terrainChunkDictionary.ContainsKey(viewedChunkCoord)) {
+                    terrainChunkDictionary[viewedChunkCoord].UpdateTerrainChunk();
+
+                    if (terrainChunkDictionary[viewedChunkCoord].IsVisible()) {
+                        visibleChunks.Add(terrainChunkDictionary[viewedChunkCoord]);
+                    }
+                } else {
+                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize));
+                }
+            }
+        }
+    }
+
+    public class TerrainChunk {
+
+        GameObject meshObject;
+        Vector2 pos;
+        Bounds bounds;
+
+        public TerrainChunk(Vector2 coord, int size) {
+            pos = coord * size;
+            Vector3 posV3 = new Vector3(pos.x, 0, pos.y);
+            bounds = new Bounds(pos, Vector2.one * size);
+
+            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject.transform.position = posV3;
+            meshObject.transform.localScale = Vector3.one * size / 10f; // plane mesh is 10 units by default
+            SetVisible(false); // default disabled
+        }
+
+        public void UpdateTerrainChunk() {
+            // find the point on perimeter closest to viewers pos
+            // find distance between point and viewer
+            // if distance is <= maxViewDist -> enable meshObject
+            // if distance is > maxViewDist -> disable meshObject
+
+            float viewerDistFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+            bool visible = viewerDistFromNearestEdge <= maxViewDist;
+            SetVisible(visible);
+        }
+
+        public void SetVisible(bool visible) {
+            meshObject.SetActive(visible);
+        }
+
+        public bool IsVisible() {
+            return meshObject.activeSelf;
+        }
+    }
+}
